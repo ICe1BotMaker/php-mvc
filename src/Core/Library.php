bad2bf10a07d60cedf4c4fa951710860930c66a1
ICe1BotMaker <ice1github@gmail.com>
2023-08-20 11:20:51 +0900

    update

--- src/Core/Library.php
+++ src/Core/Library.php
@@ -2,14 +2,17 @@
 class Library {
     public function msg_go($msg, $location) {
         echo "<script>alert('$msg'); location.href='$location';<script>";
+        exit;
     }
 
     public function msg_back($msg) {
         echo "<script>alert('$msg'); history.back();<script>";
+        exit;
     }
 
     public function json($data) {
         header("Content-type: application/json");
         echo "<pre>" . json_encode($data) . "</pre>";
+        exit;
     }
 }
